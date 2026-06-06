d20ce5f56b39841af72dc8f5f9052da6cb446e0f
Shamiul40 <shamiul4040@gmail.com>
2026-06-06 23:24:48 +0600

    prisma

--- prisma first class/src/create.ts
+++ prisma first class/src/create.ts
@@ -1,1 +1,19 @@
-console.log("this is testing")
+// Use runtime require to avoid ESM/CJS/type-definition mismatches
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const createUser = await prisma.user.create({
+    data: {
+      name: "Shamiul islam",
+      email: "shamiul@gmail.com",
+      password: "123456",
+    },
+  });
+
+  console.log(createUser, "create user response")
+}
+
+main()
