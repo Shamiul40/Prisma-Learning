4681647cfd7c6f1dbc292f714d34ba1bd032502e
Shamiul40 <shamiul4040@gmail.com>
2026-06-27 21:46:42 +0600

    style change

--- lws-prisma-crush-course-youtube/app/page.js
+++ lws-prisma-crush-course-youtube/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
+            To get started, edit t
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -38,7 +38,7 @@ export default function Home() {
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            target="_blank" 
             rel="noopener noreferrer"
           >
             <Image
